eee1024c329e9966ac997f1b95115a002312bcf1
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2025-11-17 03:53:14 +1030

    formatting

--- lib/tweak_data/upgradestweakdata.lua
+++ lib/tweak_data/upgradestweakdata.lua
@@ -7,24 +7,19 @@ Hooks:PostHook(UpgradesTweakData, "init", "upgradestweakdata_init", function(sel
 	self.values.player.body_armor.damage_shake = { 1, 0.96, 0.92, 0.85, 0.8, 0.7, 0.5 }
 	self.values.player.body_armor.stamina = { 1, 0.925, 0.90, 0.825, 0.75, 0.6, 0.5 }
 	
-	self.explosive_bullet = {
-		curve_pow = 0.5,
-		player_dmg_mul = 0.1,
-		range = 200 * 2
-	}
+	self.explosive_bullet = { curve_pow = 0.5, player_dmg_mul = 0.1, range = 200 * 2 }
 	self.explosive_bullet.feedback_range = self.explosive_bullet.range
 	
 	self.cocaine_stacks_tick_t = 4
 	self.cocaine_stacks_decay_t = 4
-	
-	
+
 	self.ecm_jammer_base_battery_life = 20
 	self.ecm_jammer_base_low_battery_life = 5
 	self.ecm_jammer_base_range = 2500
 	self.ecm_feedback_min_duration = 20
 	self.ecm_feedback_max_duration = 20
 	self.ecm_feedback_interval = 1
-	
+
 	self.ecm_feedback_retrigger_interval = 90
 	self.ecm_feedback_retrigger_chance = 1
 	
@@ -40,18 +35,9 @@ Hooks:PostHook(UpgradesTweakData, "init", "upgradestweakdata_init", function(sel
 	}
 	
 	self.loose_ammo_restore_health_values = {
-		{
-			4,
-			4
-		},
-		{
-			8,
-			8
-		},
-		{
-			12,
-			12
-		},
+		{ 4, 4 },
+		{ 8, 8 },
+		{ 12, 12 },
 		multiplier = 0.2,
 		cd = 3,
 		base = 8
@@ -73,33 +59,13 @@ Hooks:PostHook(UpgradesTweakData, "init", "upgradestweakdata_init", function(sel
 	self.skill_descs.unseen_strike.multipro = "8"
 	
 	self.values.player.detection_risk_add_crit_chance = {
-		{
-			0.01,
-			3,
-			"below",
-			35,
-			0.3
-		},
-		{
-			0.02,
-			3,
-			"below",
-			35,
-			0.3
-		}
-	}
+		{ 0.01, 3, "below", 35, 0.3 },
+		{ 0.02, 3, "below", 35, 0.3 } }
 	
 	self.skill_descs.backstab.multibasic = "1"
 	self.skill_descs.backstab.multibasic4 = "10"
 	self.skill_descs.backstab.multipro = "2"
 	self.skill_descs.backstab.multipro4 = "20"
 
-	self.values.team.crew_inspire = {
-		{
-			360,
-			360,
-			360
-		}
-	}
-
+	self.values.team.crew_inspire = { { 360, 360, 360 } }
 end)
